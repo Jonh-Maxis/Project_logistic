d3180845147df1360724e70257e6d2dfdaa55912
JonhMaxis <chaconemmanuel603@gmail.com>
2025-10-25 23:46:11 +0200

    Inicios

---
+++ Pruebas.py
@@ -0,0 +1,10 @@
+#first scrip 
+users = input("What is your name: ")
+age = input("What is your age: ")
+age = int(age)
+print(f"Is a plesure {users}, in your age is {age}")
+if age >= 18:
+   print("you are old really")
+else:
+   print("You are to young")
+   
