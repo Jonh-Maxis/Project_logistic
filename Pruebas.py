775d6f0a41ad11830d12514720ca17dbfd3d7cb6
JonhMaxis <chaconemmanuel603@gmail.com>
2025-10-29 16:35:46 +0100

    Create the database

--- Pruebas.py
+++ Pruebas.py
@@ -1,10 +1,19 @@
-#first scrip 
-users = input("What is your name: ")
-age = input("What is your age: ")
-age = int(age)
-print(f"Is a plesure {users}, in your age is {age}")
-if age >= 18:
-   print("you are old really")
-else:
-   print("You are to young")
-   
+
+import pandas as pd
+import os 
+from sqlalchemy import create_engine
+
+database_name = 'warehouse.db'
+engine = create_engine(f'sqlite:///{database_name}')
+csv_directory = 'data/archive'
+lecture = os.listdir(csv_directory)
+
+for file_name in lecture:
+   archivo = os.path.join(csv_directory,file_name)
+   read_pdf = pd.read_csv(archivo)
+   file_name = file_name.replace('.csv','').replace('_dataset','')
+   read_pdf.to_sql(
+      name=file_name,
+      con=engine,
+      if_exists='replace',
+      index=False)
